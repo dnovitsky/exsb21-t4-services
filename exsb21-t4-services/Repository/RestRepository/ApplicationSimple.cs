7bf4ce6a2456da98e17c182764cf62eda39e46c6
Nurmukhammad <nurmukhammad.sunatullaev@gmail.com>
2021-10-15 15:31:46 +0300

    Migration

---
+++ exsb21-t4-services/Repository/RestRepository/ApplicationSimple.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace exsb21_t4_services.Repository.RestRepository
+{
+    public class ApplicationSimple
+    {
+        public ApplicationSimple()
+        {
+        }
+    }
+}
