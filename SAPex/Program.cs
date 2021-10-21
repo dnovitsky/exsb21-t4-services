4e3fdd753c56311bb87cdae467061099b4425cf3
MaximPyatigor <max5good@mail.ru>
2021-10-21 12:46:25 +0300

    Add Migration Library

--- SAPex/Program.cs
+++ SAPex/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DbMigrations.Data;
+using DbMigrations.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -14,6 +18,19 @@ namespace SAPex
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            IConfigurationRoot confRoot = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).Build();
+            string connectionString = confRoot.GetConnectionString("DefaultConnection");
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            using (AppDbContext db = new AppDbContext(options))
+            {
+                db.TestTable.Add(new TestModel { Name = " first " });
+                db.SaveChanges();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- SAPex/Startup.cs
+++ SAPex/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DbMigrations.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace SAPex
                
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
