1f54743ef9453ad749c5226eb3891e4aca0685be
MaximPyatigor <max5good@mail.ru>
2021-10-22 22:05:01 +0300

    Local Server + Init in Startup

--- SAPex/Startup.cs
+++ SAPex/Startup.cs
@@ -25,7 +25,6 @@ namespace SAPex
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-               
             services.AddControllers();
             services.AddSwaggerGen();
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
@@ -60,6 +59,9 @@ namespace SAPex
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DbObjects.Initial(Configuration.GetConnectionString("DefaultConnection"));
+
         }
     }
 }
