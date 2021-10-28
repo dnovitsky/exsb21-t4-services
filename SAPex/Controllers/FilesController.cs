e9adea824f6da0819d71dbc1fcfbda989268268a
Ilya Bulakh <Bulakh_ilya@mail.ru>
2021-10-28 12:24:56 +0300

    30-create-download-files-api-endpoint-IB Done

--- SAPex/Controllers/FilesController.cs
+++ SAPex/Controllers/FilesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SAPex.Controllers
@@ -16,13 +19,28 @@ namespace SAPex.Controllers
             )
         {
             Stream stream = file.OpenReadStream();
-            string filepath = @"uploads\";
+            string filepath = @"files\";
             using (FileStream outputFileStream = new FileStream(filepath + file.FileName, FileMode.Create))
             {
                 DirectoryInfo info = new DirectoryInfo(file.ContentDisposition);
                 stream.CopyTo(outputFileStream);
                 return await Task.FromResult(Ok());
             }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> DownloadFile(int id)
+        {
+            var filePath = @"files\";
+            string existingFile = Directory.EnumerateFiles(filePath, id.ToString() + ".*").FirstOrDefault();
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(existingFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            var bytes = await System.IO.File.ReadAllBytesAsync(existingFile);
+            return File(bytes, contentType, Path.GetFileName(existingFile));
+
         }
     }
 }
