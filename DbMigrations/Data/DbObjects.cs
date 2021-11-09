007f1f9ef9ffeb9f0b48d3968239b07a7af36e11
Dzmitry Navitski <dzmitry.navitski@verint.com>
2021-11-10 01:45:51 +0300

    Updated data initialization

--- DbMigrations/Data/DbObjects.cs
+++ DbMigrations/Data/DbObjects.cs
@@ -1,12 +1,7 @@
 ﻿using DbMigrations.EntityModels;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace DbMigrations.Data
@@ -22,23 +17,19 @@ namespace DbMigrations.Data
             {
                 if (!db.Availabilities.Any())
                 {
-                    Guid id1 = new Guid("653fa839-37e8-41df-9fba-440a51466788");
-                    Guid id2 = new Guid("653fa839-37e8-41df-9fba-440a51466789");
-                    Guid id3 = new Guid("653fa839-37e8-41df-9fba-440a51466790");
-
                     db.Availabilities.Add(new AvailabilityEntityModel
                     {
-                        Id = id1,
+                        Id = Guid.NewGuid(),
                         Name = "Less than 4 hours"
                     });
                     db.Availabilities.Add(new AvailabilityEntityModel
                     {
-                        Id = id2,
+                        Id = Guid.NewGuid(),
                         Name = "4-6 hours"
                     });
                     db.Availabilities.Add(new AvailabilityEntityModel
                     {
-                        Id = id3,
+                        Id = Guid.NewGuid(),
                         Name = "6-infinity hours"
                     });
                 }
