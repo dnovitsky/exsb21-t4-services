ba022a4c31d4867d151853d4c4395dddb907aeef
Dzmitry Navitski <dzmitry.navitski@verint.com>
2021-11-10 16:47:41 +0300

    Added migration

---
+++ DbMigrations/Migrations/20211110095628_add_OrderLevel_property.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DbMigrations.Migrations
+{
+    public partial class add_OrderLevel_property : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderLevel",
+                table: "LanguageLevels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderLevel",
+                table: "AvailabilityTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderLevel",
+                table: "LanguageLevels");
+
+            migrationBuilder.DropColumn(
+                name: "OrderLevel",
+                table: "AvailabilityTypes");
+        }
+    }
+}
