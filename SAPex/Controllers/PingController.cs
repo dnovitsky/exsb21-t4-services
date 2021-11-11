84fdfe3f6a4a59139b55fb7feb83f135a921a9a2
mybook <nurmukhammad.sunatullaev@gmail.com>
2021-11-11 20:00:31 +0300

    PingController

--- SAPex/Controllers/PingController.cs
+++ SAPex/Controllers/PingController.cs
@@ -8,8 +8,42 @@ namespace SAPex.Controllers
     public class PingController : ControllerBase
     {
         [HttpGet]
-        [Authorize(Roles = "mentor")]
         public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("mentor")]
+        [Authorize(Roles = "Mentor")]
+        public ActionResult GetMentor()
+        {
+            return Ok();
+        }
+
+        [HttpGet("interviewer")]
+        [Authorize(Roles = "Interviewer")]
+        public ActionResult GetInterviewer()
+        {
+            return Ok();
+        }
+
+        [HttpGet("recruiter")]
+        [Authorize(Roles = "Recruiter")]
+        public ActionResult GetRecruiter()
+        {
+            return Ok();
+        }
+
+        [HttpGet("admin")]
+        [Authorize(Roles = "Admin")]
+        public ActionResult GetAdmin()
+        {
+            return Ok();
+        }
+
+        [HttpGet("edumanager")]
+        [Authorize(Roles = "EDU manager")]
+        public ActionResult GetEduManager()
         {
             return Ok();
         }
