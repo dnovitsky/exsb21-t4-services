43bcc580c30f95a63ba36c4106c429d24b01cf24
mybook <nurmukhammad.sunatullaev@gmail.com>
2021-11-29 14:26:08 +0300

    fixed google service

--- SAPex/Controllers/UsersController.cs
+++ SAPex/Controllers/UsersController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using AutoMapper;
 using BusinessLogicLayer.DtoModels;
 using BusinessLogicLayer.Interfaces;
+using DbMigrations.EntityModels.DataTypes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SAPex.Mappers;
 using SAPex.Models;
+using SAPex.Models.EventModels;
 
 namespace SAPex.Controllers
 {
@@ -16,12 +20,15 @@ namespace SAPex.Controllers
     [Authorize]
     public class UsersController : ControllerBase
     {
-        protected readonly UserMapper _mapper = new UserMapper();
+        private readonly IMapper _autoMapper;
         private readonly IUserService _userService;
+        private readonly IEventService _eventService;
 
-        public UsersController(IUserService userService)
+        public UsersController(IUserService userService, IEventService eventService, IMapper autoMapper)
         {
+            _autoMapper = autoMapper;
             _userService = userService;
+            _eventService = eventService;
         }
 
         [HttpGet("user-info")]
@@ -31,7 +38,7 @@ namespace SAPex.Controllers
             {
                 var email = identity.FindFirst(ClaimTypes.Email).Value;
                 var users = await _userService.FindUsersAsync(x => x.Email == email);
-                return Ok(_mapper.MapUserFromDtoToView(users.FirstOrDefault()));
+                return Ok(_autoMapper.Map<UserViewModel>(users.FirstOrDefault()));
             }
 
             return NotFound();
@@ -44,10 +51,24 @@ namespace SAPex.Controllers
 
             if (user != null)
             {
-                return await Task.FromResult(Ok(_mapper.MapUserFromDtoToView(user)));
+                return await Task.FromResult(Ok(_autoMapper.Map<UserViewModel>(user)));
             }
 
             return await Task.FromResult(NotFound());
+        }
+
+        [HttpGet("{id}/events")]
+        public async Task<IEnumerable<InterviewEventViewModel>> GetEventsByUserIdAsync(Guid id)
+        {
+            var events = await _eventService.GetAllByUserIdAsync(id);
+            return _autoMapper.Map<IEnumerable<InterviewEventViewModel>>(events);
+        }
+
+        [HttpGet("{id}/events/filter")]
+        public async Task<IEnumerable<InterviewEventViewModel>> GetUserEventsFilterAsync(Guid id, DateTime start, DateTime end, EventType type)
+        {
+            var events = await _eventService.GetAllUserFilterAsync(id, start, end, type);
+            return _autoMapper.Map<IEnumerable<InterviewEventViewModel>>(events);
         }
     }
 }
--- SAPexAuthService/Services/JwtService.cs
+++ SAPexAuthService/Services/JwtService.cs
@@ -9,6 +9,7 @@ using DbMigrations.EntityModels;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using SAPexAuthService.Models;
+using SAPexGoogleSupportService.Services.Authorization;
 // using SAPexGoogleSupportService.Services.Authorization;
 
 namespace SAPexAuthService.Services
@@ -16,23 +17,23 @@ namespace SAPexAuthService.Services
     public class JwtService
     {
         private readonly AuthUserService _userService;
-        // private readonly GoogleOAuthService _googleService;
+        private readonly GoogleOAuthService _googleService;
         private readonly AppSettingsModel _appSettings;
         private readonly AuthUserRefreshTokenService _refreshTokenService;
 
         private const string CONST_TITLE = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
 
-        public JwtService(AuthUserService userService, /* GoogleOAuthService googleService,*/  AuthUserRefreshTokenService refreshTokenService, IOptions<AppSettingsModel> appSettings)
+        public JwtService(AuthUserService userService, GoogleOAuthService googleService, AuthUserRefreshTokenService refreshTokenService, IOptions<AppSettingsModel> appSettings)
         {
             _userService = userService;
-           // _googleService = googleService;
+            _googleService = googleService;
             _appSettings = appSettings.Value;
             _refreshTokenService = refreshTokenService;
         }
 
         public async Task<TokenCredentialsModel> GoogleAuthenticateAsync(string code,string state)
         {
-            var user = new UserEntityModel(); // await _googleService.AddAsync(code,state);
+            var user = await _googleService.AddAsync(code,state);
             if (user == null)
             {
                 return null;
@@ -43,7 +44,7 @@ namespace SAPexAuthService.Services
 
         public string GetGoogleUrl(string state)
         {
-            return ""; //_googleService.GetRedirectUrl(state);
+            return _googleService.GetRedirectUrl(state);
         }
 
         public async Task<TokenCredentialsModel> AuthenticateAsync(UserCredentialsModel credentials)
