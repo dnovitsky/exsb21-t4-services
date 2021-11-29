e04d032d0cf569e7b8a693d200400660309085e7
mybook <nurmukhammad.sunatullaev@gmail.com>
2021-11-29 14:29:24 +0300

    GoogleSupportService

---
+++ SAPexGoogleSupportService/Models/Authorization/GoogleSettingsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SAPexGoogleSupportService.Models.Authorization
+{
+    public class GoogleSettingsModel
+    {
+        public string key { get; set; }
+        public string client_id { get; set; }
+        public string client_secret { get; set; }
+        public string project_id { get; set; }
+        public string auth_uri { get; set; }
+        public string token_uri { get; set; }
+        public string token_revoke_uri { get; set; }
+        public string auth_provider_x509_cert_url { get; set; }
+        public string redirect_uris { get; set; }
+        public string google_calendar_events_uri { get; set; }
+        public string google_calendar_uri { get; set; }
+        public string google_calendar_list_uri { get; set; }
+    }
+}
