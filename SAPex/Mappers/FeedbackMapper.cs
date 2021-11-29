9967b4ba9b735d54cf121210fc7e00f9a483a4cc
MikitaYepishkin <myepishkin@gmail.com>
2021-11-30 00:25:15 +0300

    106-integration-of-candidate-list. Fixed solution error

--- SAPex/Mappers/FeedbackMapper.cs
+++ SAPex/Mappers/FeedbackMapper.cs
@@ -41,7 +41,6 @@ namespace SAPex.Mappers
             var config = new MapperConfiguration(cfg => cfg.CreateMap<FeedbackDtoModel, FeedbackViewModel>()
                 .ForMember(x => x.Id, y => y.MapFrom(x => x.Id))
                 .ForMember(x => x.UserId, y => y.MapFrom(x => x.UserId))
-                .ForMember(x => x.RatingId, y => y.MapFrom(x => x.RatingId))
                 .ForMember(x => x.CreateDate, y => y.MapFrom(x => x.CreateDate))
                 .ForMember(x => x.UserReview, y => y.MapFrom(x => x.UserReview))
                 .ForMember(x => x.CandidateProccesId, y => y.MapFrom(x => x.CandidateProccesId)));
