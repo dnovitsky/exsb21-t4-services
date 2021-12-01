e044f9549065e8a1650e1ac87ad833a6e7755a4f
mybook <nurmukhammad.sunatullaev@gmail.com>
2021-12-01 10:07:37 +0300

    SchedulerService Created

---
+++ SAPexSchedulerService/Repositories/SandboxRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using SAPexSchedulerService.Data;
+using SAPexSchedulerService.Data.EntityModels;
+
+namespace SAPexSchedulerService.Repositories
+{
+    public class SandboxRepository : ISandboxRepository
+    {
+        private IDbConnection db;
+
+        public SandboxRepository(AppDbContext dapper)
+        {
+            this.db = dapper.Db;
+        }
+
+        public List<SandboxEntityModel> FindAll()
+        {
+            var sql = "SELECT *FROM Sandboxes;";
+            return db.Query<SandboxEntityModel>(sql).ToList();
+        }
+
+        public SandboxEntityModel Update(SandboxEntityModel model)
+        {
+            string query = "UPDATE Sandboxes SET Status=@Status WHERE Id = @Id";
+            db.Execute(query, new { @Status = model.Status, @Id = model.Id });
+            return model;   
+        }
+    }
+}
---
+++ SAPexSchedulerService/Services/StatusService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using SAPexSchedulerService.Data.Base;
+using SAPexSchedulerService.Repositories;
+
+namespace SAPexSchedulerService.Services
+{
+    public class StatusService : IStatusService
+    {
+        private readonly ISandboxRepository _repository;
+        public StatusService(ISandboxRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void StatusJob()
+        {
+            var now = DateTime.Now;
+            var sandboxes = _repository.FindAll();
+            foreach (var sandbox in sandboxes)
+            {
+                if (sandbox.StartRegistration <= now && now < sandbox.EndRegistration)
+                {
+                    sandbox.Status = StatusName.Registration;
+                }
+                else if (sandbox.EndRegistration <= now && now < sandbox.StartDate)
+                {
+                    sandbox.Status = StatusName.Application;
+                }
+                else if (sandbox.StartDate <= now && now < sandbox.EndDate)
+                {
+                    sandbox.Status = StatusName.Inprogress;
+                }
+                else if(sandbox.EndDate < now)
+                {
+                    sandbox.Status = StatusName.Archive;
+                }
+
+                _repository.Update(sandbox);
+            }
+        }
+    }
+}
