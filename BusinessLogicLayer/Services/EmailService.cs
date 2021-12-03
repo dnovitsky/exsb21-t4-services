204f00b66f6f172d9032ff62f567546e284beb9d
mybook <nurmukhammad.sunatullaev@gmail.com>
2021-12-03 13:46:56 +0300

    Email Service Updated

--- BusinessLogicLayer/Services/EmailService.cs
+++ BusinessLogicLayer/Services/EmailService.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using AutoMapper;
+using BusinessLogicLayer.Interfaces;
+using DataAccessLayer.Service;
+
 namespace BusinessLogicLayer.Services
 {
-    public class EmailService
+    public class EmailService : IEmailService
     {
-        public EmailService()
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EmailService(IUnitOfWork unitOfWork, IMapper mapper)
         {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
         }
     }
 }
