0c14dfa9bb714c79856c0947ae71ea51a1270cc9
mybook <nurmukhammad.sunatullaev@gmail.com>
2021-12-03 10:37:06 +0300

    EmailsTableCreated

---
+++ DbMigrations/EntityModels/DataTypes/EmailStatusType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DbMigrations.EntityModels.DataTypes
+{
+    public enum EmailStatusType
+    {
+        None = 0,
+        ReadyForSend = 1,
+        InProcess = 2,
+        Sent = 3
+    }
+}
